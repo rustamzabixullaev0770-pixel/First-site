61af038a68fb4f429ae17c51799806707a7d818c
rustamzabixullaev0770-pixel <rustamzabixullaev0770@gmail.com>
2026-02-01 23:58:39 +0500

    Home Events

--- src/components/HomeEventsCards.jsx
+++ src/components/HomeEventsCards.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import BorderBtn from './BorderBtn'
 
-export default function HomeEventsCards() {
+export default function HomeEventsCards({number , month, time, description, sDescription}) {
   return (
-    <div>
-      
+    <div className='grid grid-cols-20 px-4 py-6 bg-white rounded-lg homeEvent items-center'>
+      <div className='col-span-1 text-[#FF3F3A] text-5xl font-bold'>{number}</div>
+      <div className='col-span-3 px-4'>
+        <p className='text-[#1E212C] text-xl font-bold'>{month}</p>
+        <p className='text-[#787A80 ]'>{time}</p>
+      </div>
+      <div className='col-span-13'>
+        <p className='text-[#1E212C] font-bold'>{description}</p>
+        <p className='text-[#787A80]'>{sDescription}</p>
+      </div>
+      <div className='col-span-3'>
+        <BorderBtn text_2="View more" link_2="/events" />
+      </div>
     </div>
   )
 }
--- src/modules/home modules/HomeEents.jsx
+++ src/modules/home modules/HomeEents.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import HomeEventsCards from '../../components/HomeEventsCards'
+import PrimaryBtn from '../../components/PrimaryBtn'
 
 export default function HomeEvents() {
+
+  const events = [
+    {
+      number: "05",
+      month: "August",
+      time: "11:00 - 14:00",
+      description: "Formation of the organizational structure of the company in the face of uncertainty.",
+      sDescription: "Onine master-class"
+    }, {
+      number: "24",
+      month: "July",
+      time: "11:00 – 12:30",
+      description: "Building a customer service department. Best Practices.",
+      sDescription: "Onine lecture"
+    }, {
+      number: "05",
+      month: "July",
+      time: "10:00 – 13:00",
+      description: "How to apply methods of speculative design in practice. Worldbuilding prototyping.",
+      sDescription: "Onine workshop"
+    }
+  ]
+
   return (
-    <div style={{ backgroundImage: `url(src/assets/home/home_events-bg.png)`, backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className='px-40 py-12'>
-      <p className='text-[#1E212C] font-bold text-center'>Our Events</p>
-      <h4 className='text-[#1E212C] text-5xl font-bold text-center'>Lectures & workshops</h4>
+    <div className='bg-[url(src/assets/home/home_events-bg.png)] object-cover bg-center'>
+      <div className='px-40 pt-34 pb-24'>
+        <div>
+          <p className='text-[#1E212C] font-bold text-center'>Our Events</p>
+          <h4 className='text-[#1E212C] text-5xl font-bold text-center'>Lectures & workshops</h4>
+        </div>
+        <div className='flex flex-col gap-8 my-16'>
+          {
+            events.map(event => (
+              <HomeEventsCards number={event.number} month={event.month} time={event.time} description={event.description} sDescription={event.sDescription} />
+            ))
+          }
+        </div>
+        <div className='flex justify-center gap-10 mt-16 items-center'>
+          <h2 className='text-[#1E212C] text-3xl font-bold'>Do you want more?</h2>
+          <PrimaryBtn text="Explore all events" link="/events" />
+        </div>
+      </div>
     </div>
   )
 }
